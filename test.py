2b662e5a8bc1ba22a133ede30cf859248c8173f8
mininerej <ejhong.4991@gmail.com>
2018-04-26 04:49:13 +0900

    .

--- test.py
+++ test.py
@@ -17,41 +17,42 @@ time_consume = started_at
 
 while True:
     study_started = True
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
+    # if not video_capture.isOpened():
+    #     print('Unable to load camera.')
+    #     sleep(5)
+    #     pass
 
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if ret:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    if anterior != len(faces):
-        anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
-
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        if anterior != len(faces):
+            anterior = len(faces)
+            log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
 
 # When everything is done, release the capture
 video_capture.release()
